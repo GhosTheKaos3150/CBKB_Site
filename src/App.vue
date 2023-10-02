e5c54fe85fd999d1180b5d022c5150afb42162ea
GhosTheKaos3150 <uriel_anderson@hotmail.com>
2023-10-02 16:00:35 -0300

    alterações no SEO

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,38 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import { useMeta } from 'quasar';
+
+const metaData = {
+  title: 'Meditar em Fortaleza - Centro Budista Kadampa Bodhisattva',
+
+  meta: {
+    description: {
+      name: 'description',
+      content:
+        'O Centro Bodhisattva está aberto a todas as pessoas que buscam a paz interior. ' +
+        ' Oferecemos um espaço tranquilo e acolhedor, aberto a budistas e não budistas.',
+    },
+    keywords: {
+      name: 'keywords',
+      content:
+        'budismo moderno em fortaleza, budismo kadampa em fortaleza, meditar em fortaleza, budismo em fortaleza, kadampa em fortaleza, geshe-la em fortaleza,' +
+        'pujas em fortaleza, centro de meditação em fortaleza, meditação em fortaleza, meditação kadampa em fortaleza, budismo no ceará, budismo kadampa no ceará,' +
+        'kadampa no ceará, centro budista kadampa bodhisattva',
+    },
+  },
+
+  // <noscript> tags
+  noscript: {
+    default: 'Por favor, instale ou habilite o JavaScript em seu Browser!',
+  },
+};
+
 export default defineComponent({
   name: 'App',
+
+  setup() {
+    useMeta(metaData);
+  },
 });
 </script>
